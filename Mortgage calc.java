ba3b37bfd93fb27c73b294514dc815e39d4b8b24
Omprakash Ganesan <omprakashganesan@iMac.local>
2021-08-31 22:10:46 -0700

    Om made change

--- Mortgage calc.java
+++ Mortgage calc.java
@@ -6,7 +6,8 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	final byte MONTHS_IN_YEAR=12;
+	final byte MONTHS_IN_YEAR=13
+;
         final byte PERCENT=100;
         Scanner scanner=new Scanner(System.in);
         System.out.print("principal");
