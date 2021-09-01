9b74f43b8e8972aa17469550f326b687bbac9f2c
Omprakash Ganesan <omprakashganesan@iMac.local>
2021-08-31 21:43:05 -0700

    Om changes

--- Palindrome.java
+++ Palindrome.java
@@ -4,7 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         int r, sum = 0, temp;
-        int n = 5651;
+        int n = 1000;
         temp = n;
         {
             while(n >= 0) ;
